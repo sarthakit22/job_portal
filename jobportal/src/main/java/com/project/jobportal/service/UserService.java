f632dd06d6b3eedde5a5a5e9cb8b8e1f503d364a
sarthakit22 <sarthakarya.it22@gmail.com>
2022-04-13 10:58:46 +0530

    Update UserService.java

--- jobportal/src/main/java/com/project/jobportal/service/UserService.java
+++ jobportal/src/main/java/com/project/jobportal/service/UserService.java
@@ -2,6 +2,7 @@ package com.project.jobportal.service;
 
 import java.util.regex.Pattern;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
